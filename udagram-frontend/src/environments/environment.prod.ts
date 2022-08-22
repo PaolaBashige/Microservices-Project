2cb7682f5c7fad73b14b7fb23bbb169206f390c9
Paola Bashige <paolabashige@gmail.com>
2022-08-22 13:14:06 +0300

    CI and CD Commit

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://acaa36fb2e31d4ebbb22d47a789da5b3-761053320.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
